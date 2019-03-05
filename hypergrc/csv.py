daf869fff3f2d479c6dd28ec2fbae7e72a3e6ac7
Peter Kaminski <kaminski@istori.com>
2019-03-05 14:29:08 -0500

    first pass at hacking in CSV output/export

---
+++ hypergrc/csv.py
@@ -0,0 +1,45 @@
+# Construct system security plans from project data.
+
+from . import opencontrol
+
+def build_csv(project, options):
+
+  # create buffer for output
+  from io import StringIO
+  buf = StringIO()
+
+  # Load the standards in use by this project.
+  standards = opencontrol.load_project_standards(project)
+
+  # Collect all of the control narratives.
+  narratives = []
+  for component in opencontrol.load_project_components(project):
+    # Iterate over its controls...
+    for controlimpl in opencontrol.load_project_component_controls(component, standards):
+      # If only one control family is requested, then skip others.
+      if options.get("only-family"):
+        if controlimpl["family"]["abbrev"] != options["only-family"]:
+          continue
+
+      # Add the narrative to the list of narratives to output.
+      narratives.append(controlimpl)
+
+  # Sort the narratives by standard, family, control, part, and then by component.
+  narratives.sort(key = lambda narrative : (
+    narrative["standard"]["name"],
+    narrative["family"]["sort_key"],
+    narrative["control"]["sort_key"],
+    narrative["control_part"] is not None, # narratives for the null part go first
+    narrative["control_part"],
+    narrative["component"]["name"] )
+  )
+
+  # Write the narratives to CSV.
+  import csv
+  csvwriter = csv.writer(buf, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+  csvwriter.writerow(["Standard Name", "Control Part", "Component Name"])
+  for narrative in narratives:
+#    if narrative["control_part"] is not None:
+      csvwriter.writerow([narrative["standard"]["name"], narrative["control_part"], narrative["component"]["name"]])
+
+  return buf.getvalue()
